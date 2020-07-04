62430f84c3b3013f40eece50623f8eda0522686a
cyf <1106572144@qq.com>
2020-07-04 15:28:35 +0800

    second

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,22 @@
+// pages/demo03/demo03.js
+Page({
+
+  data: {
+  msg: "hello mina",
+  num: 10000,
+  list:[
+    {
+      id:0,
+      name:"生活"
+    },
+    {
+      id:1,
+      name:"南京市第六届"
+    },
+    {
+      id:2,
+      name:"极乐世界的"
+    }
+  ]
+  }
+});
---
+++ pages/demo04/demo04.js
@@ -0,0 +1,12 @@
+// pages/demo04/demo04.js
+Page({
+
+  data: {
+   num:0
+  },
+  handleInput(e){
+    this.setData({
+       num:e.detail.value
+    })
+  }
+})
